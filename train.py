0b9c84eced391f5d8f51be5dcce3f0bdb58b21b2
XYPB <duyx2000@gmail.com>
2021-03-31 17:46:03 +0800

    still using 64*64

--- train.py
+++ train.py
@@ -14,9 +14,14 @@ parser.add_argument('--param_path', type=str, default='model/colorization_releas
 parser.add_argument('--batch_size', type=int, default=32)
 parser.add_argument('--output_dir', type=str, default='imgs_out/')
 parser.add_argument('--num_epoch', type=int, default=10)
+parser.add_argument('--img_size', type=int, default=64)
 
 if __name__ == '__main__':
 	opt = parser.parse_args()
+	transformer = transforms.Compose([
+        transforms.Resize(opt.img_size),
+        transforms.RandomHorizontalFlip(),
+    ])
 
 	device = 'cuda' if torch.cuda.is_available() else 'cpu'
 	tr_loader = tinycoco_dataset.get_TinyCOCO_loader(root=opt.data_path, batch_size=opt.batch_size, task='train')
